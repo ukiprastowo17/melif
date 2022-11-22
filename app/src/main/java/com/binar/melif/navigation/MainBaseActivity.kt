cd51c70f8034e5409d2209e4a378c7883e70689f
RodanDuamar <muhammadrodan9@gmail.com>
2022-11-22 14:49:01 +0700

    Add logic for Main Activity(Base)

--- app/src/main/java/com/binar/melif/navigation/MainBaseActivity.kt
+++ app/src/main/java/com/binar/melif/navigation/MainBaseActivity.kt
@@ -2,11 +2,39 @@ package com.binar.melif.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.binar.melif.R
 import com.binar.melif.base.BaseActivity
 import com.binar.melif.databinding.ActivityMainBaseBinding
+import com.binar.melif.navigation.fragment.FirstFragment
+import com.binar.melif.navigation.fragment.SecondFragment
 
 class MainBaseActivity : BaseActivity<ActivityMainBaseBinding>(ActivityMainBaseBinding::inflate){
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+//        val chatActivity = Intent(this@MainBaseActivity, ChatActivity::class.java)
+
+        setCurrentFragment(firstFragment)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.movie -> setCurrentFragment(firstFragment)
+                R.id.tvshow -> setCurrentFragment(secondFragment)
+//                R.id.chat -> startActivity(chatActivity)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_fragment, fragment)
+            commit()
+        }
+    }
 
 }
