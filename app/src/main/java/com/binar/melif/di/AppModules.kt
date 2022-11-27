9cfdd1417ef112733b73af8099da33dfc3b9de7a
RodanDuamar <muhammadrodan9@gmail.com>
2022-11-27 15:19:59 +0700

    Logic Authentication feature

--- app/src/main/java/com/binar/melif/di/AppModules.kt
+++ app/src/main/java/com/binar/melif/di/AppModules.kt
@@ -1,20 +1,37 @@
 package com.binar.melif.di
 
 import com.binar.melif.BuildConfig
+import com.binar.melif.auth.AuthViewModel
+import com.binar.melif.data.firebase.FirebaseUserAuthDataSourceImpl
+import com.binar.melif.data.firebase.UserAuthDataSource
+import com.binar.melif.data.repository.UserRepository
+import com.binar.melif.data.repository.UserRepositoryImpl
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.gson.Gson
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 object AppModules {
 
     fun getModules(): List<Module> = listOf(
-         common, firebase
+         common, firebase, dataSource, repository, viewModels
     )
 
+    private val dataSource = module {
+        single<UserAuthDataSource> { FirebaseUserAuthDataSourceImpl(get()) } // singleton
+    }
 
+    private val repository = module {
+        single<UserRepository> { UserRepositoryImpl(get()) } // singleton
+    }
+
+    private val viewModels = module {
+        viewModelOf(::AuthViewModel)
+    }
 
     private val common = module {
         single { Gson() }
